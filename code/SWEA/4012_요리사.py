2f1d331e016079f60b75d84a179129384e6ebc91
kyeah01 <kyeah01@gmail.com>
2019-03-18 11:35:35 +0900

    190318 | SWEA_4012_cooker

--- code/SWEA/4012_요리사.py
+++ code/SWEA/4012_요리사.py
@@ -1,8 +1,8 @@
 def combination(arr, r):
-    arr = sorted(arr)
     def generate(chosen):
+        global answer
         if len(chosen) == r:
-            print(chosen)
+            answer += [chosen[:]]
             return
         start = arr.index(chosen[-1]) + 1 if chosen else 0
         for nxt in range(start, len(arr)):
@@ -10,15 +10,22 @@ def combination(arr, r):
             generate(chosen)
             chosen.pop()
     generate([])
-
-
-combination('ABCDE', 2)
-combination([1, 2, 3, 4, 5], 3)
-
+    return answer
 
 T = int(input())
 for tc in range(1, T+1):
+    answer = []
     N = int(input())
     synergy = [list(map(int, input().split())) for _ in range(N)]
-    check = [0 for _ in range(N)]
-    
+    A_cases = combination(list(range(N)), N//2)
+    B_cases = [[i for i in range(N) if i not in A] for A in A_cases]
+    minN = 40000
+    for k in range(len(A_cases)):
+        A_ingredient, B_ingredient = 0,0
+        A_case, B_case = A_cases[k], B_cases[k]
+        for i in range(N//2):
+            for j in range(i+1, N//2):
+                A_ingredient += synergy[A_case[i]][A_case[j]]+synergy[A_case[j]][A_case[i]]
+                B_ingredient += synergy[B_case[i]][B_case[j]]+synergy[B_case[j]][B_case[i]]
+        minN = min(minN, abs(A_ingredient-B_ingredient))
+    print(minN)
