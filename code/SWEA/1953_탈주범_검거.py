86837ebaaa304fb7c5300bc57f1697094205452b
kyeah01 <kyeah01@gmail.com>
2019-03-20 19:59:23 +0900

    190320 | SWEA 1953, 2005

--- code/SWEA/1953_탈주범_검거.py
+++ code/SWEA/1953_탈주범_검거.py
@@ -1,20 +1,21 @@
-def navi(value, i, j, time):
-    global pipe, N, M
+import sys
+sys.stdin = open('input.txt')
+
+def navi(i, j, time):
+    global cnt
     if not time:
-        return 
-    way = pipe[value]
+        return
+    way = pipe[maps[i][j]]
+    maps[i][j] = 0
+    cnt += 1
     for d in range(len(way)):
         x = i+way[d][0]
         y = j+way[d][1]
-        print(x,y)
-        # range over가 나지 않으면서, maps[x][y]가 0이 아니면서, 두 파이프가 연결된 경우. 
         if 0<=x<N and 0<=y<M and maps[x][y] and [-way[d][0], -way[d][1]] in pipe[maps[x][y]]:
-            if not check[x][y]:
-                check[x][y] = 'v'
-                return navi(maps[x][y],x,y,time-1)
+            navi(x,y,time-1)
 
 pipe = {
-    1 : [[0,1],[1,0],[0,-1],[-1,0]],
+    1 : [[-1,0],[0,1],[1,0],[0,-1]],
     2 : [[-1,0],[1,0]],
     3 : [[0, -1], [0, 1]],
     4 : [[-1, 0],[0,1]],
@@ -27,9 +28,8 @@ T = int(input())
 for tc in range(1, T+1):
     N, M, R, C, L = map(int, input().split())
     maps = [list(map(int, input().split())) for _ in range(N)]
-    print(maps)
-    check = [[0 for _ in range(M)] for _ in range(N)]
-    check[R][C] = 'v'
-    navi(maps[R][C],R,C,L)
+    cnt = 0
+    navi(R,C,L)
+    print(cnt)
     for i in range(N):
-        print(*check[i])
+        print(*maps[i])
---
+++ code/SWEA/2005_파스칼의_삼각형.py
@@ -0,0 +1,9 @@
+T = int(input())
+
+for tc in range(1, T+1):
+    n = int(input())
+    i = 1
+    while i <= n:
+        if i == 1:
+            tri += [1]
+        
