a670baec3ec1d9e15536b5ff1c6a6d9bcbdbfe60
kyeah01 <kyeah01@gmail.com>
2019-03-22 18:06:17 +0900

    190322 | BOJ 1260

--- code/Baekjoon/1260_DFS와_BFS.py
+++ code/Baekjoon/1260_DFS와_BFS.py
@@ -8,11 +8,16 @@ for i in range(M):
     edg[input_edges[i][1]] += [input_edges[i][0]]
 
 queue = [V]
-start = V
-visited = []
+BFS_visited = []
 
 while queue:
-    visited += [queue.pop(0)]
-    queue += [e for e in edg[visited[-1]] if e not in visited]
+    BFS_visited += [queue.pop(0)]
+    queue += [e for e in edg[BFS_visited[-1]] if e not in BFS_visited and e not in queue]
 
-print(visited)
+print(BFS_visited)
+
+stack = [V]
+DFS_visited = []
+
+# while True:
+    
