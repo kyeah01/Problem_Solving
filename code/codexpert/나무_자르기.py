95ca58ed0f8efdfc88e621071574696785152a15
kang yewon <kyeah01@gmail.com>
2019-03-29 08:54:38 +0900

    190329 | binary

--- code/codexpert/나무_자르기.py
+++ code/codexpert/나무_자르기.py
@@ -1,29 +1,36 @@
 def finder(n, m, trees):
     def sumit(val):
-        result = 0
-        for i in range(n-1, 0, -1):
-            if trees[i] <= val:
-                break
-            result += trees[i]-val
+        # result = 0
+        # for i in range(n-1, 0, -1):
+        #     if trees[i] <= val:
+        #         break
+        #     result += trees[i]-val
+        result = sum(trees[i]-val for i in range(n) if trees[i]>val)
         return result
 
     def binary(m):
-        s, e = 0, max(trees)
+        s, e = 0, max(trees)+1
         while s < e:
             mid = (s+e)//2
             result = sumit(mid)
+            if s+1 == e:
+                return e
             if result == m:
                 return mid
             if result < m:
-                e = mid+1
+                e = mid
             else:
-                sol = mid
                 s = mid
         return sol
     return binary(m)
 
+# 1과 2사이의 값을 찾아야 할때
+# 그럼 값을 1.**로 나오게 만들어보자
+# 
+
+
 # main
 n, m = map(int, input().split())
 trees = list(map(int, input().split()))
-trees.sort()
+# trees.sort()
 print(finder(n,m,trees))
