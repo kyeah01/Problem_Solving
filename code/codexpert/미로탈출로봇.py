bd1be21b254646bd9313100031fdc1e2c3d78297
kang yewon <kyeah01@gmail.com>
2019-03-31 17:03:29 +0900

    190331 | codexpert BFS

--- code/codexpert/미로탈출로봇.py
+++ code/codexpert/미로탈출로봇.py
@@ -2,15 +2,23 @@ def Quick_finder(start, end):
     direction = [0,0,1,-1]
     queue = [start]
     while queue:
-        queue.pop(0)
+        i,j,t = queue.pop(0)
         for d in range(4):
-            
+            x = i+direction[d]
+            y = j+direction[::-1][d]
+            if 0<=x<x_size and 0<=y<y_size and not maps[x][y]:
+                if (x,y) == end:
+                    print(t+1)
+                    return t+1
+                if (x,y) not in queue:
+                    queue += [(x,y, t+1)]
+                    maps[x][y] = 1
 
 # main
 # x가 가로, y가 세로.
-x_size, y_size = map(int, input().split())
-Sx, Sy, Ex, Ey = map(int, input().split())
-maps = [list(map(int, input()) for _ in range(y_size)]
-
-# 시작점을 큐에 저장하고
-# 
+y_size, x_size = map(int, input().split())
+Sy, Sx, Ey, Ex = map(int, input().split())
+maps = [list(map(int, input())) for _ in range(x_size)]
+start = (Sx-1, Sy-1, 0)
+end = (Ex-1, Ey-1)
+Quick_finder(start,end)
